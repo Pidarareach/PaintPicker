ec354f24e4997b36656eee9ece7e72d08f76bda9
Pidarareach Mom <117335222+Pidarareach@users.noreply.github.com>
2025-05-20 12:42:29 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿//using System.Diagnostics;
+namespace PaintPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isRandom;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (!isRandom)
+            {
+                var red = SliderRed.Value;
+                var green = SliderGreen.Value;
+                var blue = SliderBlue.Value;
+
+                Color color = Color.FromRgb(red, green, blue);
+
+                SetColor(color);
+            }
+            
+        }
+
+        private void SetColor(Color color)
+        {
+            //Debug.WriteLine(color.ToString());
+            btnRandom.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            isRandom = true;
+
+            var random = new Random();
+
+            var color = Color.FromRgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+            SetColor(color);
+
+            SliderRed.Value = color.Red;
+            SliderGreen.Value = color.Green;
+            SliderBlue.Value = color.Blue;
+            isRandom = false;
+        }
+    }
+
+}
